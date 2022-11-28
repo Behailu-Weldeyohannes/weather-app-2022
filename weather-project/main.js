e3cab9cf7ebd641b9488e48b52e14f219ef5ccf4
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2022-11-28 13:25:42 -0500

    landing-page, and structure has been added

--- weather-project/main.js
+++ weather-project/main.js
@@ -1,53 +1,8 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header-container");
 
-
-const apiKey = "4d8fb5b93d4af21d66a2948710284366";
-
-let weather = {
-  apiKey,
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
-    document.querySelector(".city").innerText =
-      "The Current Weather in " + name + " is ";
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "℃";
-  
-    document.querySelector(".weather").classList.remove("loading");
-    
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
 };
 
-// search
-
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
-
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
-
-weather.fetchWeather("Washington DC");
+mobile_nav.addEventListener("click", () => toggleNavbar());
